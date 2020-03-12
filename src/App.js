d0e1fee8723324c47dd1cacaf3b55758b0cbb565
Lee Peters <lpeters0023@gmail.com>
2020-03-12 16:48:31 -0400

    Thursdays awesome work map bounds and ran num

--- src/App.js
+++ src/App.js
@@ -3,20 +3,74 @@ import React from 'react';
 //import leafletPip from 'leaflet-pip'; 
 import Maplet from './Map.js';
 import './App.css';
+let randLat;
+let randLng;
+let center; 
 
 class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      start: false,
+      quit: false,
+      zoom: 8, 
+      score: 100,
+      center: [44.0886, -72.7317],
+    }
+  }
+  getRandomLat = () => { // return random latitute to a lot of decimal places (maybe trim later?)
+    let lat = Math.random() * (45.005419 - 42.730315) + 42.730315;
+    return lat;
+}
+getRandomLng = () => { // return random longitude to a lot of decimal places (maybe trim later?)
+  let lng = (Math.random() *(71.510225 - 73.35218) + 73.35218) * -1;
+  return lng;
+}
+  startGame = () => {
+    this.setState({
+      start: true,
+      quit: false,
+      zoom: 8,
+      center: [randLat, randLng]
+
+    }) 
+      randLat = this.getRandomLat()
+      console.log(randLat)
+      randLng = this.getRandomLng()
+      console.log(randLng)
+  }
+
+  makeGuess = () => {
+    // take input from play in the form of a dropped pin
+    // take coordinates from pin and check against random pin coordinates
+    // clickable county menu appears 
+    // check lat long against inner polygon and/ or county code
+    // if correct, add points to score & alert is displayed 
+    // if incorrect, subtract 10pts from score
+  }
+
+  quitGame = () => {
+    this.setState({
+      quit: true,
+      start: false, 
+      // info panel displays randomNum
+      // and correct town & county is displayed
+      // setTimeout(3000, resets page)
+    })
   }
 
   render() {
     return(
       <div>
-        <Maplet id="maplet" />
+        <button id="startButton" className="button" onClick={this.startGame} disabled={this.state.start}> Start Game</button>
+        <button id="quitButton" className="button" onClick={this.quitGame} disabled={!this.state.start}>Quit</button>
+        <button id="guessButton" className="button" onClick={this.makeGuess}disabled={!this.state.start}>Guess</button>
+         <Maplet id="maplet" />
       </div>
     )
   }
 }
+
+
 
 export default App;
--- src/Map.js
+++ src/Map.js
@@ -1,35 +1,26 @@
 import React, { Component } from 'react'
-import {Map, TileLayer, Marker, Popup} from 'react-leaflet'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
 import L from 'leaflet'
 import borderData from './border.js'
 
 class Maplet extends Component {
 
-state = {
-  lat: 44.0886,
-  lng: -72.7317,
-  zoom: 8,
+  componentDidMount() {
+    this.map = L.map('map', {
+      center: [44.0886, -72.7317],
+      zoom: 8,
+      layers: [L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x})',
+    { attribution : 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community' }),
+  ],
+    gameStart: false
+  })
+  L.geoJSON(borderData).addTo(this.map) 
 }
 
-componentDidMount() {
-
-}
-
-  render() {
-    const position = [this.state.lat, this.state.lng] 
-    return (
-        <Map center={position} zoom={this.state.zoom}>
-          <TileLayer
-            url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-            attribution= 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-          />
-          <Marker position={position}>
-            <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
-          </Marker>
-        </Map>
-    )
+render() {
+  return <div id='map' />
   }
-}
 
+}
 export default Maplet
 
